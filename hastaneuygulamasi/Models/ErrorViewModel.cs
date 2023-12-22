15f2dbf000db9759d222d9daca4ff42c9110846b
elifsevvalsacli <90870934+elifsevvalsacli@users.noreply.github.com>
2023-12-22 22:21:32 +0300

    commit1

---
+++ hastaneuygulamasi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace hastaneuygulamasi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
